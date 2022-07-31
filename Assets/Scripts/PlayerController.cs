97db867ef505fd06bbee16b287cd640efb13469d
akankshreddy <60999380+akankshreddy@users.noreply.github.com>
2022-07-31 11:34:53 +0530

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D theRB;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Transform groundPoint;
+    private bool isOnGround;
+    public LayerMask whatIsGround;
+        
+        
+	public Animator anim; 
+
+
+	public BulletController shotToFire;
+	public Transform shotPoint;
+
+	private bool canDoubleJump;
+
+
+
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+    	
+		//move sideways
+        theRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed , theRB.velocity.y);
+
+		//handle direction change
+		if(theRB.velocity.x < 0)
+		{
+			transform.localScale = new Vector3 (-1f , 1f , 1f);
+			
+			//transform.Rotate(0f ,180f ,0f );
+		}else if (theRB.velocity.x > 0)
+		{
+			transform.localScale = Vector3.one;
+		}
+		
+		
+		//jumping 
+		if(Input.GetButtonDown("Jump") && isOnGround)
+		{
+			if(isOnGround)
+			{
+				canDoubleJump = true;
+
+			}
+			else
+			{
+				canDoubleJump = false; 
+				
+				anim.SetTrigger("doubleJump");
+			}
+			
+				
+			theRB.velocity = new Vector2(theRB.velocity.x , jumpForce);
+			
+		}
+		
+			
+		if(Input.GetButtonDown("Fire1"))
+		{
+		
+			Instantiate(shotToFire,shotPoint.position ,shotPoint.rotation).moveDir = new Vector2(transform.localScale.x , 0f);
+			
+			anim.SetTrigger("shotFired");
+		}
+			
+			
+		//checking if on the ground
+		isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
+        
+            if(Input.GetButtonDown("Jump") && isOnGround)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+        
+        
+	    anim.SetBool("isOnGround" , isOnGround);
+	    anim.SetFloat("speed" , Mathf.Abs(theRB.velocity.x));
+    }
+}
